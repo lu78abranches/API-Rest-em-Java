6f74b65999fd443800fd3d1b1b24eca2e28ba4c8
lu78abranches <luisabranches.surf@hotmail.com>
2023-07-14 00:25:52 -0300

    Ordenação e paginação das rerquisições.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,9 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,8 +28,8 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {//argumentos para paginação e ordenação da requisição, Troca da List por page, do return foi retirado os métodos stream() e toList()
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 }
