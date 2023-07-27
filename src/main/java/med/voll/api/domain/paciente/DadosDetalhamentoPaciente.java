c3ad5f789b5751daa4630c81247a4a8f7ca9e0ec
lu78abranches <luisabranches.surf@hotmail.com>
2023-07-26 23:35:26 -0300

    Tratamento de erros

--- src/main/java/med/voll/api/domain/paciente/DadosDetalhamentoPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosDetalhamentoPaciente.java
@@ -1,7 +1,7 @@
-package med.voll.api.controller.paciente;
+package med.voll.api.domain.paciente;
 
-import med.voll.api.endereco.Endereco;
-import med.voll.api.paciente.Paciente;
+import med.voll.api.domain.endereco.Endereco;
+import med.voll.api.domain.paciente.Paciente;
 
 
 public record DadosDetalhamentoPaciente(Long id, String nome, String email, String telefone, String cpf, Endereco endereco) {
